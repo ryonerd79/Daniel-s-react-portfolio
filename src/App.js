3ff5df36df9bdd3bd571946398ab2aba7bc03c7f
Muramori71 <dannyroh2500@gmail.com>
2023-08-21 12:56:39 -0700

    ran npx and added files to the src folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './About';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <main>
+      </main>
+      <Footer /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav>
+     <a href="#">About Me</a>
+     <a href="#">Portfolio</a>
+     <a href="#">Contact</a>
+     <a href="#">Resume</a>
+    </nav>
+  )
+}
+
+export default Navigation
