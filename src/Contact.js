6a406639a860e05b1e7123ee81937f750610484e
Muramori71 <dannyroh2500@gmail.com>
2023-08-23 13:37:30 -0700

    updated and created files

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Contact() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [err, setErr] = useState("")
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!name) {
+      setErr('Name cannot be empty')
+    } else if(!email.includes('@')) {
+      setErr('Please provide valid email')
+    } else if(!message) {
+      setErr('Message cannot be empty')
+    }
+  }
+ 
   return (
-    <div>Contact</div>
+    <div className='contact_page'>
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+       <label>Name</label>
+       <input type="text" onChange={(e)=> setName(e.target.value)} />
+       <label>Email Address</label>
+       <input type="email" onChange={(e)=> setEmail(e.target.value)}/>
+       <label>Message</label>
+       <textarea onChange={(e)=> setMessage(e.target.value)}>
+        Message here
+        </textarea>
+        {err && <p>{err}</p>}
+      <button>Submit</button> 
+      </form>
+    </div>
   )
 }
 
--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <div class="resume-info">
+    <h2>Resume</h2>
+    <p>This is my resume</p>
+    <h2>Front-end</h2>
+    <ul>
+     <li>HTML</li>
+     <li>CSS</li>
+     <li>javaScript</li> 
+    </ul>
+    <h2>Back-end</h2>
+    <ul>
+     <li>API</li>
+     <li>Sequelize</li>
+     <li>Express</li>
+     <li>React</li> 
+     </ul>
+    </div>
   )
 }
 
