a33a8979f89f16f76bfddd1345b0222046630439
Muramori71 <dannyroh2500@gmail.com>
2023-08-24 15:05:55 -0700

    added a third profile to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
       <p>
         <a href="https://github.com/ryonerd79" target='_blank' rel="noreferrer"><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/daniel-roh-37a612123/" target='_blank' rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.facebook.com/daniel.roh.12/" target='_blank' rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
       </p>
 
     </footer>
